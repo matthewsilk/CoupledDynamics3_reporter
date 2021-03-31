ae91235b884488f43b06dedf0ee769bd8456cbb4
Simon <simon@elisya.org>
2021-03-31 10:32:09 -0400

    more documnetation

--- tools.R
+++ tools.R
@@ -1,10 +1,12 @@
 
-#@param param a dataframe with at least the path to the output of the simultion a a column attributing the value of  _var_ for each simulationj
-#@param measure which measure do we want (in "concern"   "exps"      "infs"      "hosps"     "recovers"  "deaths"    "cases" "newcases" "repdeaths" "reports" )
-#@param metrics  
-#@param metrics  
-#@param maxt longest simulation time expected, if NULL this will correspond to the time of longest simulations within the one selected.
-getDataTrajByVar <- function(param,measure="cases",maxt=NULL,metric="colSums",var,probs=c(.25,.50,.75)){
+#' @param param a dataframe with at least the path to the output of the simultion a a column attributing the value of  _var_ for each simulationj
+#' @param measure which measure do we want (in "concern"   "exps"      "infs"      "hosps"     "recovers"  "deaths"    "cases" "newcases" "repdeaths" "reports" )
+#' @param metrics by default: colSums which will sum the value of the measures for all cuminities
+#' @param maxt longest simulation time expected, if NULL this will correspond to the time of longest simulations within the one selected.
+#' @param probs the quantile we wants from the simulations 
+#' @return a list of of size \code{length(var)} storing matrices of \code{dim=c(maxt,length(probs))}
+# note: not sure this was the wised way to do, I guess should be splitted by `var` at previous level
+getDataTrajByVar <- function(param,measure="infs",maxt=NULL,metric="colSums",var,probs=c(.25,.50,.75)){
     allsim=sapply(param$id,function(i)getAllInf(i,measure,metric)) #get all 
     if(is.null(maxt))maxt=max(lengths(allsim)) #get the longest simulation time
     ##concacatenate all simulation using "var"
@@ -14,41 +16,45 @@ getDataTrajByVar <- function(param,measure="cases",maxt=NULL,metric="colSums",va
     })
     names(allvar)=unique(param[[var]])
     return(allvar)
-
 }
 
+#' functions that take  matrices of \code{dim=c(t,c)} (where t is the length of a simulation and c the number of community in the simulation) and that return some metrics out of thes matrices
 whenpeak <- function(d)which.max(colSums(d))
 peak <- function(d)max(colSums(d))
 sd_peak <- function(d)sd(apply(d,1,which.max)) #variation in community peak
 
 
 
-#@param df a datafram
-#@param exp an expression as returned by expression or bquote or quote  
+#' @param df a dataframe
+#' @param exp an expression as returned by expression or bquote or quote  
+#' @return a subset of the dataframe \code{df} respecting the expression \code{exp}
 getSubset <- function(df,exp)return(subset(df,eval(exp)))
 
 
-#@param param a dataframe with at least the path to the output of the simultion a a column attributing the value of  _var_ for each simulationj
-#@param measure which measure do we want (in "concern"   "exps"      "infs"      "hosps"     "recovers"  "deaths"    "cases" "newcases" "repdeaths" "reports" )
-#@param metrics  
+#' @param summaries a list of matrics stories summaries of trajectories
+#' @param 
+#' @param 
 plotTrajByVar <- function(summaries,cls=NULL,maxt=NULL,posl="topright",add=F,legend=F){
     if(is.null(cls)){
         cls=adjustcolor(1:length(summaries),.5)
         names(cls)=names(summaries)
     }
     rangey=range(summaries)
-    if(!add)plot(1,1,type="n",ylim=rangey,xlim=c(1,ncol(summaries[[1]]))) 
+    if(!add)plot(1,1,type="n",xlab="time",ylab="",ylim=rangey,xlim=c(1,ncol(summaries[[1]]))) 
     lapply(seq_along(summaries),function(l)lines(summaries[[l]][2,],col=cls[l]))
     lapply(seq_along(summaries),function(l)lines(summaries[[l]][1,],col=cls[l],lty=2))
     lapply(seq_along(summaries),function(l)lines(summaries[[l]][3,],col=cls[l],lty=2))
     if(legend)legend(posl,legend=unique(names(summaries)),col=cls,lwd=1)
 }
 
-#@param param a dataframe with at least the path to the output of the simultion a a column attributing the value of  _var_ for each simulationj
+#' @param param a dataframe with at least the path to the output of the simultion a a column attributing the value of  _var_ for each simulationj
 #' @param measures one or more measure stored in a simulation
 #' @param metrics one or more functions that can be applied to retrieve information from the simulations
-getUniqueMetrics <- function(param,measures="cases",metrics="max"){
-    sapply(param$id,function(i)getAllInf(i,measures,metrics))
+#' @return a table storing for each simulation the metrics computed on the measures wanted 
+getMetrics <- function(param,measures="cases",metrics="max"){
+    res=sapply(param$id,function(i)getAllInf(i,measures,metrics))
+    if(!is.null(dim(res)))return(t(res))
+    return(res)
 }
 
 #' Return simulation output from a given expe id
